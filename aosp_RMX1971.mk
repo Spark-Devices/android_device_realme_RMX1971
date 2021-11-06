b619ecaf6a5ae5040b2df3459c90190dc24dac4f
KrishWalecha <Krishwalecha098@gmail.com>
2021-11-06 08:11:10 +0000

    RMX1971: ProjectElixir Bringup

--- aosp_RMX1971.mk
+++ aosp_RMX1971.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifer: Apache-2.0
 #
 
-# Inherit some common PixelExperience stuff.
+# Inherit some common ProjectElixir stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from RMX1971 device.
@@ -16,16 +16,16 @@ TARGET_GAPPS_ARCH := arm64
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := RMX1971
 PRODUCT_NAME := aosp_RMX1971
-PRODUCT_BRAND := Realme
-PRODUCT_MODEL := Realme 5 Pro
-PRODUCT_MANUFACTURER := Realme
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := realme 5 Pro
+PRODUCT_MANUFACTURER := realme
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sdm710-user 9 PKQ1.190101.001 eng.root.20191220.123812 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SD1A.210817.015.A4 7697517 release-keys"
 
-BUILD_FINGERPRINT := "Realme/RMX1971/RMX1971:9/PKQ1.190101.001/1576816457:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SD1A.210817.015.A4/7697517:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="RMX1971" \
