fb61cd38145ce2080ae36aba61aaf3888a9fd1aa
kssrao13882 <kssrao13882@gmail.com>
2023-01-03 13:28:46 +0530

    RMX1971: SparkOS Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_RMX1971.mk
+    $(LOCAL_DIR)/spark_RMX1971.mk
+
+COMMON_LUNCH_CHOICES := \
+    spark_RMX1971-user \
+    spark_RMX1971-userdebug \
+    spark_RMX1971-eng
